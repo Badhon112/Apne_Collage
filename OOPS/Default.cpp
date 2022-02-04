871fb5467bc702479cbe0ed89fd5c0b0b0288592
Badhon <biswasbadhon896@gmail.com>
2022-02-05 00:18:00 +0600

    Default

--- OOPS/Default.cpp
+++ OOPS/Default.cpp
@@ -13,17 +13,55 @@ public:
         name = "NULL";
         age = 0;
         height = 0.0f;
+    };
+    person(string name_a, int age_a, float height_a)
+    {
+        name = name_a;
+        age = age_a;
+        height = height_a;
     }
     void valid()
     {
-        cout << name << endl;
-        cout << age << endl;
-        cout << height << endl;
+        cout << "Name : " << name << endl;
+        cout << "Age : " << age << endl;
+        cout << "Height : " << height << endl;
     }
 };
 int main()
 {
-    person P;
-    P.valid();
-    cout<<"Done"<<endl;
+    // person P,
+    // P1("Badhon", 20, 5.7),
+    // P2("Durjoy", 25, 5.6),
+    // P3("Mahabub", 30, 5.5),
+    // P4("Mafus", 22, 5.4);
+    // P.valid();
+    // P1.valid();
+    // P2.valid();
+    // P3.valid();
+    // P4.valid();
+    string name;
+    int age;
+    float height;
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     person P[i];
+    // }
+    for (int i = 0; i < 3; i++)
+    {
+        // person P[i];
+        // cout << "Name : ";
+        cin >> name;
+        // cout << endl;
+        // cout << "Age : ";
+        cin >> age;
+        // cout << endl;
+        // cout << "Height : ";
+        cin >> height;
+        // cout << endl;
+        person P(name, age, height);
+        P.valid();
+    }
+    // for(int i=0;i<3;i++){
+    //     P[i].v
+    // }
 }
