b73ef055931ce15431b6b50aa4ee739aa8960698
Badhon <biswasbadhon896@gmail.com>
2022-02-04 23:13:48 +0600

    Old Code Again

---
+++ OOPS/A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Class
+class house
+{
+    //Member variable
+private:
+    int length = 0, breath = 0;
+
+    //Member Function
+public:
+    void setData(int x, int y)
+    {
+        length = x;
+        breath = y;
+    }
+    void area()
+    {
+        cout << "Area of that House : " << length * breath << endl;
+    }
+};
+
+int main()
+{
+    house h1;
+    int a,b;
+    cin>>a>>b;
+    h1.setData(a, b);
+    h1.area();
+}
